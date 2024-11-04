c1cafc48a84ea5b78eb2e996259aeffbc29cc661
sunkenquest <sunkenquest@gmail.com>
2024-11-04 16:39:52 +0800

    feat: add base frontend for the blog

---
+++ cms-frontend/src/components/atoms/BlogContentAtom.vue
@@ -0,0 +1,25 @@
+<template>
+    <p class="content" :class="props.customClass">
+        <slot />
+    </p>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    customClass: {
+        type: String,
+        default: '',
+    },
+})
+</script>
+
+<style scoped>
+.content {
+    font-size: 1rem;
+    line-height: 1.6;
+    color: #555;
+    margin-bottom: 1rem;
+}
+</style>
