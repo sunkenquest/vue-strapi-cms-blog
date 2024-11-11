f396089ed75eb691acb9e01ed4b328499ac9a9f5
m_.kco <83852126+sunkenquest@users.noreply.github.com>
2024-11-11 10:02:12 +0800

    chore: add description to the newsletter (#9)

--- cms-frontend/src/components/molecules/BlogNewsLetterMolecule.vue
+++ cms-frontend/src/components/molecules/BlogNewsLetterMolecule.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="my-5 max-w-7xl px-6 lg:px-8">
+        <h1 class="mx-10 my-2">Subscribe to get future posts via email</h1>
         <form class="mx-auto flex max-w-md gap-x-4" @submit.prevent="handleSubmit">
             <BlogInputFieldAtom id="email-address" name="email" type="email" placeholder="Type your email..."
                 v-model="email" background="bg-white/5" ring="white/10" textColor="text-white" />
